876a13b9573c1f2493e0abb001296dfcf4a02ae8
si3aa <mahmoud22said22@gmail.com>
2024-06-21 16:21:18 +0300

    create Zekr_item_widget

--- lib/ui/screens/home_screen/tabs/azkar/zekr_items.dart
+++ lib/ui/screens/home_screen/tabs/azkar/zekr_items.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class ZekrItems extends StatefulWidget {
+  const ZekrItems({super.key});
+
+  @override
+  State<ZekrItems> createState() => _ZekrItemsState();
+}
+
+class _ZekrItemsState extends State<ZekrItems> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: const Center(
+        child: Text("Zekr Items"),
+      ),
+    );
+  }
+}
